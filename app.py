8cd679e56b3d4ca1f90c73ddc9a4d88f853b90cf
Adis Hadzic <adis7hadzic@gmail.com>
2021-01-20 15:25:16 +0100

    basic form

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home(name="Lucija Babić"):
-    return render_template("index.html", name=name)
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        guest = request.form["nm"]
+        return redirect(url_for("user", no=guest))
+    else: 
+        return render_template("index.html")
+
+@app.route("/<no>")
+def user(no):
+    return f"<h1>Number of guests: {no}</h1>"
 
 if __name__== '__main__':
     app.run(port=5000, debug=True)
