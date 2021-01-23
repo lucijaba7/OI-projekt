d3ddf8906e060601ee9b3f8c0e226ece4e1059d5
Kaja Radošević <73023084+KajaRado7@users.noreply.github.com>
2021-01-23 14:21:50 +0100

    Uređenje Index.html i App.py(u doradi)

--- app.py
+++ app.py
@@ -6,15 +6,17 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         guest = request.form["nm"]
-        return redirect(url_for("user", no=guest))
+        price = request.form["nmm"]
+        return redirect(url_for("user", no=guest,nmm=price))
     else: 
         return render_template("index.html")
 
-@app.route("/<no>")
-def user(no):
-    return f"<h1>Number of guests: {no}</h1>"
 
-    
 
+@app.route("/output/<no><nmm>")
+def user(no,nmm):
+    #Dorada output-a
+    return f"<h1>Broj turista : {int(no)}</h1> </br> <h1>Max cijena : {int (nmm)}</h1>"
+  
 if __name__== '__main__':
     app.run(port=5000, debug=True)
