e54d12952ef4fa1c0f85adecf994da2b56b78b48
Lucia Varesko <lucia3varesko@gmail.com>
2021-01-25 20:39:39 +0100

    transfer solved

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, url_for, render_template, request
+import transfer as t
 
 app = Flask(__name__)
+guest = ""
+price = ""
+ 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
         guest = request.form["nm"]
         price = request.form["nmm"]
-        return render_template("ispis.html", guest=guest, price=price)
+        return render_template("ispis.html", rezultat=t.transfer(guest), guest=guest, price=price)
     else:
         return render_template("index.html")
 
