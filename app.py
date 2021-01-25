75ece02fce05998209a9d5f1143e88518771dfa8
Lucia Varesko <lucia3varesko@gmail.com>
2021-01-25 18:48:07 +0100

    dohvat podataka proba

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ def home():
     if request.method == "POST":
         guest = request.form["nm"]
         price = request.form["nmm"]
-        return redirect(url_for("user", no=guest,nmm=price))
-    else: 
+        return f"<h1>Broj turista : { guest } </h1> </br> <h1>Max cijena : { price }</h1>"
+    else:
         return render_template("index.html")
 
 
 
-@app.route("/output/<no><nmm>")
-def user(no,nmm):
+@app.route("/podaci")
+def user():
     #Dorada output-a
-    return f"<h1>Broj turista : {int(no)}</h1> </br> <h1>Max cijena : {int (nmm)}</h1>"
+    return render_template("ispis.html")
   
 if __name__== '__main__':
     app.run(port=5000, debug=True)
